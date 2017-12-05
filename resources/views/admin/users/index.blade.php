b34968abe74ea1684b1ed14c68311527cf554d41
Adrian Ivanciu <adrianivanciu@yahoo.com>
2017-12-05 15:03:24 +0000

    users section working 100% -ecept security

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -9,6 +9,7 @@
   <thead>
     <tr>
       <th scope="col">Id</th>
+        <th scope="col">Photo</th>
       <th scope="col">Name</th>
        <th scope="col">Email</th>
         <th scope="col">Role</th>
@@ -22,12 +23,13 @@
       @foreach($users as $user)
   <tr>
       <th scope="row">{{$user->id}}</th>
-      <td>{{$user->name}}</td>
-      <td>{{$user->email}}</td>
-      <td>{{$user->role->name}}</td>
-      <td>{{$user->is_active==1?'Active':'Not Active'}}</td>
-      <td>{{$user->created_at->diffForHumans()}}</td>
-      <td>{{$user->updated_at->diffForHumans()}}</td>
+      <th scope="row"><img height="50" src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" alt=""></th>
+      <td scope="row"><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
+      <td scope="row">{{$user->email}}</td>
+      <td scope="row">{{$user->role->name}}</td>
+      <td scope="row">{{$user->is_active==1?'Active':'Not Active'}}</td>
+      <td scope="row">{{$user->created_at->diffForHumans()}}</td>
+      <td scope="row">{{$user->updated_at->diffForHumans()}}</td>
     </tr>
 
   @endforeach
